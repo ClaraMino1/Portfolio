2a661660d3e2227e835976741a7429a89092bd20
Clara Miño <claraa.mino12@gmail.com>
2026-04-11 22:44:51 -0300

    basic structure

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+// import { Hero } from './features/hero/Hero';
+// import { Trajectory } from './features/trajectory/Trajectory';
+// import { Skills } from './features/skills/Skills';
+// import { Projects } from './features/projects/Projects';
+// import { Contact } from './features/contact/Contact';
+// import { Footer } from './components/Footer';
+
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+      {/* <Hero />
+      <Trajectory />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer /> */}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+const NavBar = () => {
+  return (
+    <h1>NAV BAR</h1>
+  )
+}
+
+export default NavBar;
